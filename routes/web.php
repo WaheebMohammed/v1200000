f51f7cdfc53142c2126ea31de7917c3f238f1882
WaheebMohammed <WaheebHamouda@gmail.com>
2023-08-01 10:45:09 +0300

    msg

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function () {
+    return view('welcome');
+});
+
+
+/**
+ * git add .
+ * git commit -m "msg"
+ * git push
+ */
